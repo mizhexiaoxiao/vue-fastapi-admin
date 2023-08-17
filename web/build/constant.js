665611aad7c96d49ac0f890ce9b026a5b97cd8ec
mizhexiaoxiao <1157861072@qq.com>
2023-08-17 14:58:35 +0800

    Update Web BaseAPI

--- web/build/constant.js
+++ web/build/constant.js
@@ -1,23 +1,23 @@
 export const OUTPUT_DIR = 'dist'
 
 export const PROXY_CONFIG = {
-  /**
-   * @desc    替换匹配值
-   * @请求路径  http://localhost:3100/api/user
-   * @转发路径  http://localhost:9999/api/v1 +/user
-   */
-  '/api': {
-    target: 'http://localhost:9999/api/v1',
-    changeOrigin: true,
-    rewrite: (path) => path.replace(new RegExp('^/api'), ''),
-  },
   // /**
-  //  * @desc    不替换匹配值
-  //  * @请求路径  http://localhost:3100/api/v1/user
-  //  * @转发路径  http://localhost:9999/api/v1/user
+  //  * @desc    替换匹配值
+  //  * @请求路径  http://localhost:3100/api/user
+  //  * @转发路径  http://localhost:9999/api/v1 +/user
   //  */
-  // '/api/v1': {
-  //   target: 'http://localhost:9999',
+  // '/api': {
+  //   target: 'http://localhost:9999/api/v1',
   //   changeOrigin: true,
+  //   rewrite: (path) => path.replace(new RegExp('^/api'), ''),
   // },
+  /**
+   * @desc    不替换匹配值
+   * @请求路径  http://localhost:3100/api/v1/user
+   * @转发路径  http://localhost:9999/api/v1/user
+   */
+  '/api/v1': {
+    target: 'http://localhost:9999',
+    changeOrigin: true,
+  },
 }
