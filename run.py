1f5684d74629a2c1f19d1852038e4915503d7c3e
mizhexiaoxiao <1157861072@qq.com>
2023-08-18 16:15:05 +0800

    uvicorn add loggin config

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="0.0.0.0", port=9999, reload=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=9999, reload=True, log_config="uvicorn_loggin_config.json")
--- run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/zsh
-
-uvicorn app:app --reload --host 0.0.0.0 --port 9999
