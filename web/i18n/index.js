caa78bda51621f8b825ec192af1bd80132632a13
QuangNN <nquang17@gmail.com>
2023-11-01 23:40:10 +0700

    update language select dropdown

--- web/i18n/index.js
+++ web/i18n/index.js
@@ -1,12 +1,14 @@
 import { createI18n } from 'vue-i18n'
+import { sStorage } from '@/utils'
 
 import messages from './messages'
 
+const currentLocale = sStorage.get('locale')
 
 const i18n = createI18n({
   legacy: false,
   globalInjection: true,
-  locale: 'en',
+  locale: currentLocale || 'en',
   fallbackLocale: 'en',
   messages: messages
 })
--- web/src/layout/components/header/components/Languages.vue
+++ web/src/layout/components/header/components/Languages.vue
@@ -1,5 +1,5 @@
 <template>
-  <n-dropdown :options="options" @select="handleChangeLocale" v-model="locale">
+  <n-dropdown :options="options" @select="handleChangeLocale">
     <n-icon mr-20 size="18" style="cursor: pointer">
       <icon-mdi:globe/>
     </n-icon>
@@ -8,18 +8,23 @@
 
 <script setup>
 import {useI18n} from 'vue-i18n'
+import {useAppStore} from "@/store";
 
-const {availableLocales, locale,} = useI18n()
+const store = useAppStore()
+const { availableLocales, t } = useI18n()
 
-const options = []
-availableLocales.forEach(locale => {
-  options.push({
-    label: locale,
-    key: locale
+const options = computed(() => {
+  let select = []
+  availableLocales.forEach(locale => {
+    select.push({
+      label: t('lang', 1, {'locale': locale}),
+      key: locale
+    })
   })
+  return select
 })
 
 const handleChangeLocale = (value) => {
-  locale.value = value
+  store.setLocale(value)
 }
 </script>
