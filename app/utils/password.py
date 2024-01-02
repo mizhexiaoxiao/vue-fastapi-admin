dc47a3a79b758b25fcaa735d870959a9776d9749
mizhexiaoxiao <1157861072@qq.com>
2024-01-02 17:59:44 +0800

    Update password hashing algorithm

--- app/utils/password.py
+++ app/utils/password.py
@@ -1,7 +1,7 @@
 from passlib import pwd
 from passlib.context import CryptContext
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
 
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
