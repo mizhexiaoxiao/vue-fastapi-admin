6b4e6829a1e0592b8d2e0a492470af20943a7ec6
HanWenLu <38583116+hanwenlu2016@users.noreply.github.com>
2025-07-05 08:25:22 +0800

    Update auditlog.py

--- app/api/v1/auditlog/auditlog.py
+++ app/api/v1/auditlog/auditlog.py
@@ -4,21 +4,23 @@ from tortoise.expressions import Q
 
 from app.models.admin import AuditLog
 from app.schemas import SuccessExtra
+from app.schemas.apis import *
 
 router = APIRouter()
 
 
 @router.get("/list", summary="查看操作日志")
 async def get_audit_log_list(
-        page: int = Query(1, description="页码"),
-        page_size: int = Query(10, description="每页数量"),
-        username: str = Query("", description="操作人名称"),
-        module: str = Query("", description="功能模块"),
-        method: str = Query("", description="请求方法"),
-        summary: str = Query("", description="接口描述"),
-        status: int = Query(None, description="状态码"),
-        start_time: datetime = Query("", description="开始时间"),
-        end_time: datetime = Query("", description="结束时间")
+    page: int = Query(1, description="页码"),
+    page_size: int = Query(10, description="每页数量"),
+    username: str = Query("", description="操作人名称"),
+    module: str = Query("", description="功能模块"),
+    method: str = Query("", description="请求方法"),
+    summary: str = Query("", description="接口描述"),
+    path: str = Query("", description="请求路径"),
+    status: int = Query(None, description="状态码"),
+    start_time: datetime = Query("", description="开始时间"),
+    end_time: datetime = Query("", description="结束时间"),
 ):
     q = Q()
     if username:
@@ -29,6 +31,8 @@ async def get_audit_log_list(
         q &= Q(method__icontains=method)
     if summary:
         q &= Q(summary__icontains=summary)
+    if path:
+        q &= Q(path__icontains=path)
     if status:
         q &= Q(status=status)
     if start_time and end_time:
@@ -38,10 +42,7 @@ async def get_audit_log_list(
     elif end_time:
         q &= Q(created_at__lte=end_time)
 
-    # Apply pagination without ordering
-    query = AuditLog.filter(q)
-
-    audit_log_objs = await query.offset((page - 1) * page_size).limit(page_size)
+    audit_log_objs = await AuditLog.filter(q).offset((page - 1) * page_size).limit(page_size).order_by("-created_at")
     total = await AuditLog.filter(q).count()
     data = [await audit_log.to_dict() for audit_log in audit_log_objs]
     return SuccessExtra(data=data, total=total, page=page, page_size=page_size)
